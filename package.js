2eb2c402d8fc1366c32a1f31796dfb76d68bcc91
Jaired Jawed <jairedjawed@Jaireds-MacBook-Pro.local>
2018-12-28 04:20:21 -0800

    Update Readme.md

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'jaireddjawed:flow-router-react-helpers',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'Makes Flow Router function like react router.',
   // URL to the Git repository containing the source code for this package.
@@ -12,13 +12,13 @@ Package.describe({
   documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.7.0.5');
   api.use('ecmascript');
   api.mainModule('flow-router-react-helpers.js');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('ecmascript');
   api.use('tinytest');
   api.use('kadira:flow-router@2.12.1');
